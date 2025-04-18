7ed1d82425cbb335139e46c316ab5a00557ba1b0
Sara Martín <saramartinfuentes@hotmail.com>
2025-04-18 21:00:09 +0200

    experiment created

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@
 //                                                                    //  
 ////////////////////////////////////////////////////////////////////////
 
-import { correctObjects, incorrectObjects } from "./objects.js";
+import { menDataArray, womenDataArray, menNames, womenNames } from "./objects.js";
 
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
 
 /**************************************************************************************/
@@ -25,26 +25,9 @@ if (randomNumber < 0.5) {
 
 /**************************************************************************************/
 
-const PEOPLE_URL =
-  "https://raw.githubusercontent.com/saramff/face-images-spanish/refs/heads/master";
-const IMAGES_PER_GENDER = 24;  
 
-// Create pictures arrays for men and women images
-const menImages = Array.from(
-  { length: IMAGES_PER_GENDER },
-  (_, i) => `${PEOPLE_URL}/men/man_${i + 1}.jpg`
-);
-const womenImages = Array.from(
-  { length: IMAGES_PER_GENDER },
-  (_, i) => `${PEOPLE_URL}/women/woman_${i + 1}.jpg`
-);
 
-// Create new array concatenating men & women images
-const peopleImages = [...menImages, ...womenImages];
 
-// Create name arrays for men and women
-const menNames = ["Antonio", "José", "Manuel", "Juan", "Francisco", "David", "Javier", "Carlos", "Miguel", "Jesús", "Alejandro", "Pedro", "Pablo", "Ángel", "Sergio", "Rafa", "Andrés", "Alberto", "Enrique", "Rubén", "Fernando", "Luis", "Daniel", "Marcos"];
-const womenNames = ["María", "Carmen", "Ana", "Laura", "Isabel", "Marta", "Sara", "Elena", "Lucía", "Patricia", "Teresa", "Pilar", "Silvia", "Rosa", "Beatriz", "Claudia", "Raquel", "Sonia", "Nuria", "Alicia", "Irene", "Andrea", "Cristina", "Julia"];
 
 // Create suffle function - suffles array index randomly
 function shuffle(array) {
@@ -55,159 +38,38 @@ function shuffle(array) {
 }
 
 // Suffle all arrays (images and names)
-shuffle(menImages);
-shuffle(womenImages);
+shuffle(menDataArray);
+shuffle(womenDataArray);
 shuffle(menNames);
 shuffle(womenNames);
 
-// Create object array for men and women {img, name} 
-const menImgsNames = menImages.map((img, index) => {
-  return {
-    img: img,
-    name: menNames[index],
-  };
-});
+// Add names to men and women 
 
-const womenImgsNames = womenImages.map((img, index) => {
-  return {
-    img: img,
-    name: womenNames[index],
-  };
-});
+menDataArray.forEach((man, index) => man.name = menNames[index]);
+womenDataArray.forEach((woman, index) => woman.name = womenNames[index]);
 
-// Create new array concatenating men & women images & names
-const peopleImgsNames = [...menImgsNames, ...womenImgsNames];
+// Create new array concatenating men & women
+const peopleDataArray = [...menDataArray, ...womenDataArray];
 
-// suffle people imgs & names array randomly
-shuffle(peopleImgsNames);
+// suffle people array randomly
+shuffle(peopleDataArray);
+
+// Get images to preload them
+const bodyImgs = peopleDataArray.map((person) => person.bodyImg);
+const faceImgs = peopleDataArray.map((person) => person.faceImg);
+const allImgs = [...bodyImgs, ...faceImgs];
 
 
 /**************************************************************************************/
 
-// add correct_response to objects array
-correctObjects.forEach((object) => object.correct_response = correctKey);
-
-// create objects images array to preload them
-const objectsImgs = correctObjects.map((object) => object.img);
-
-// Create function to get a new array with a random slice from other array
-function getRandomSlice(array, sliceSize) {
-  const arraySlice = [];
-
-  for (let i = 0; i < sliceSize; i++) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const randomPerson = array.splice(randomIndex, 1)[0];
-    arraySlice.push(randomPerson);
-  }
-
-  return arraySlice;
-}
-
-// Define slice size & create men & women array copy not to alter the original ones
-const SLICE_SIZE = 24;
-const menCopy = [...menImgsNames];
-const womenCopy = [...womenImgsNames];
-
-// Create men & women slice array
-const menSlice = getRandomSlice(menCopy, SLICE_SIZE);
-const womenSlice = getRandomSlice(womenCopy, SLICE_SIZE);
-
-// Create correct objects slice array
-const correctObjectsSlice = getRandomSlice(correctObjects, SLICE_SIZE)
-
-// Add correct objects to men & women
-const menCorrectObjects = correctObjectsSlice.slice(0, SLICE_SIZE / 2);
-const womenCorrectObjects = correctObjectsSlice.slice(SLICE_SIZE / 2, correctObjectsSlice.length);
-
-menCorrectObjects.forEach((object, index) => menSlice[index].object = object);
-womenCorrectObjects.forEach((object, index) => womenSlice[index].object = object);
-
-// Create incorrect objects slice (first is just sentences, and then random images are added)
-const incorrectObjectsSlice = getRandomSlice(incorrectObjects, SLICE_SIZE);
-
-const incorrectObjectsWithImg = incorrectObjectsSlice.map((objectSentence) => {
-  const img = getRandomSlice(correctObjects, 1)[0].img;
-
-  return {
-    sentence: objectSentence,
-    img: img,
-    correct_response: incorrectKey
-  }
-})
-
-// Add incorrect objects to men & women
-const menIncorrectObjects = incorrectObjectsWithImg.slice(0, SLICE_SIZE / 2);
-const womenIncorrectObjects = incorrectObjectsWithImg.slice(SLICE_SIZE / 2, incorrectObjectsSlice.length);
-
-menIncorrectObjects.forEach((object, index) => menSlice[menSlice.length - 1 - index].object = object);
-womenIncorrectObjects.forEach((object, index) => womenSlice[womenSlice.length - 1 - index].object = object);
-
-// Create people slice array concatenating men & women slice arrays
-const peopleSlice = [...menSlice, ...womenSlice];
-
-// Shuffle people slice array
-shuffle(peopleSlice);
 
 
 /**************************************************************************************/
 
-const NEW_PEOPLE_URL =
-  "https://raw.githubusercontent.com/saramff/face-images-spanish/refs/heads/master/new-faces/newface_";
-const NEW_IMAGES = 48;
 
-// Create pictures array for new images
-const newImages = Array.from(
-  { length: NEW_IMAGES },
-  (_, i) => {
-    return {
-      img: `${NEW_PEOPLE_URL}${i + 1}.jpg`,
-      correct_response: incorrectKey
-    }
-  }
-);
-
-const peopleSliceImgs = peopleSlice.map((person) => {
-  return {
-    img: person.img,
-    correct_response: correctKey
-  }
-})
-
-const recognitionFaces = [...newImages, ...peopleSliceImgs];
-
-shuffle(recognitionFaces);
-
-// create faces images array to preload them
-const recognitionFacesImgs = recognitionFaces.map((face) => face.img);
 
 /**************************************************************************************/
 
-const NAMES_PER_GENDER = 24
-
-const newMenNames = ["Víctor", "Álvaro", "Jorge", "Iván", "Guillermo", "Adrián", "Diego", "Óscar", "Samuel", "Cristian", "Jaime", "Hugo", "Tomás", "Ignacio", "Nicolás", "Mario", "Lorenzo", "Rodrigo", "Esteban", "Gonzalo", "Mateo", "Martín", "Joaquín", "Julián"];
-const newWomenNames = ["Alba", "María José", "Noelia", "Belén", "Carolina", "Inés", "Ángela", "Manuela", "Lourdes", "Eugenia", "Paula", "Verónica", "Rocío", "Mercedes", "Esther", "Gema", "Ainhoa", "Lidia", "Vanesa", "Miriam", "Eva", "Lola", "Antonia", "Marina"];
-const newNames = [...newMenNames, ...newWomenNames];
-
-const newNamesWithResponse = newNames.map((name) => {
-  return {
-    name: name,
-    correct_response: correctKey
-  }
-})
-
-const menNamesSlice = getRandomSlice([...menNames], NAMES_PER_GENDER);
-const womenNamesSlice = getRandomSlice([...womenNames], NAMES_PER_GENDER);
-const menAndWomenSliceNames = [...menNamesSlice, ...womenNamesSlice];
-
-const menAndWomenNamesWithResponse = menAndWomenSliceNames.map((name) => {
-  return {
-    name: name,
-    correct_response: incorrectKey
-  }
-})
-
-const allNames = [...newNamesWithResponse, ...menAndWomenNamesWithResponse];
-shuffle(allNames);
 
 
 /**************************************************************************************/
@@ -352,23 +214,10 @@ timeline.push(demo2);
 /* Preload images */
 let preload = {
   type: jsPsychPreload,
-  images: peopleImages,
+  images: allImgs,
 };
 timeline.push(preload);
 
-/* Preload objects */
-let preloadObjects = {
-  type: jsPsychPreload,
-  images: objectsImgs,
-};
-timeline.push(preloadObjects);
-
-/* Preload new faces */
-let preloadNewFaces = {
-  type: jsPsychPreload,
-  images: recognitionFacesImgs,
-};
-timeline.push(preloadNewFaces);
 
 /* Fixation trial */
 let fixation = {
@@ -390,132 +239,9 @@ let welcome = {
 timeline.push(welcome);
 
 
-// /**************************************************************************************/
-
-/* Instructions trial */
-let instructions = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: `
-    <p>En este experimento se mostrarán automáticamente diferentes rostros uno tras otro.</p>  
-    <p>Por favor, preste mucha atención a cada rostro y al nombre que lo acompaña.</p>
-    <p>Los rostros aparecerán automáticamente y no necesita hacer nada más que estar atento.</p>
-    <p>Cuando esté preparado, pulse la barra espaciadora para empezar.</p>
-  `,
-  choices: [' '],
-  post_trial_gap: 500,
-};
-timeline.push(instructions);
-
-/* Create stimuli array for image presentation */
-let test_stimuli = peopleImgsNames.map((person) => {
-  return {
-    stimulus: `
-      <img class="person-img" src="${person.img}">
-      <p class="person-name">${person.name}</p>
-    `,
-  };
-});
-
-/* Image presentation trial */
-let test = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: jsPsych.timelineVariable("stimulus"),
-  choices: "NO_KEYS", // Prevent key press
-  trial_duration: 1000, // Display each image for 1 second
-  post_trial_gap: 500
-};
-
-/* Test procedure: fixation + image presentation */
-let test_procedure = {
-  timeline: [fixation, test],
-  timeline_variables: test_stimuli,
-  randomize_order: true, // Randomize image order
-};
-timeline.push(test_procedure);
-
-
 /**************************************************************************************/
 
-/* Instructions for recognition phase */
-let instructionsrecognition = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: `
-    <p>Ahora verá una serie de rostros junto con un objeto y una frase asociada.</p>
-    <p>Presione '${incorrectKey.toUpperCase()}', si la frase es falsa, y '${correctKey.toUpperCase()}', si la frase es verdadera.</p>
-    </p></p>
-    <p>Como en este ejemplo: si en la pantalla aparecen la cara de Ana y un oso de peluche, y la frase dice 'Ana tiene un bolígrafo', presione '${incorrectKey.toUpperCase()}' (NO).</p>
-    <br />
-    <div>
-      <img src='https://raw.githubusercontent.com/saramff/face-recognition-images/refs/heads/master/Example/Ana.jpg'  class="img-instructions" />
-      <img src='https://raw.githubusercontent.com/saramff/face-recognition-images/refs/heads/master/Example/Teddy.jpg' class="img-instructions" />
-    </div>
-    <br />
-    <p>Le recomendamos colocar los dedos sobre las teclas ${correctKey.toUpperCase()} y ${incorrectKey.toUpperCase()} durante la tarea para no olvidarlas.</p>
-    <p>Cuando esté preparado, pulse la barra espaciadora para empezar.</p>
-   `,
-  choices: [' '],
-  post_trial_gap: 500,
-};
-timeline.push(instructionsrecognition);
 
-/* Create stimuli array for object presentation */
-let test_objects_stimuli = peopleSlice.map((person) => {
-  return {
-    noFaceStimulus: `
-      <p class="person-name">Es ${person.name}.</p>
-      <div class="imgs-container">
-        <img class="person-img" src="${person.img}">
-      </div>
-    `,
-    stimulus: `
-    <p class="person-name">Es ${person.name}.</p>
-    <div class="imgs-container">
-      <img class="person-img" src="${person.img}">
-      <img class="object-img" src="${person.object.img}">
-    </div>
-    <p class="person-name">${person.name} ${person.object.sentence}</p>
-    <div class="keys">
-      <p class="${correctKey === 'a' ? 'left' : 'right'}">SÍ</p>
-      <p class="${correctKey === 'a' ? 'right' : 'left'}">NO</p>
-    </div>
-  `,
-    correct_response: person.object.correct_response
-  };
-});
-
-/* Only Face trial */
-let onlyFace = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: jsPsych.timelineVariable("noFaceStimulus"),
-  choices: "NO_KEYS", // Prevent key press
-  trial_duration: 2000, // Only Face duration
-};
-
-/* Object presentation trial */
-let testObjects = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: jsPsych.timelineVariable("stimulus"),
-  choices: ['a', 'l'],
-  data: {
-    task: "response object presentation",
-    correct_response: jsPsych.timelineVariable("correct_response"),
-  },
-  on_finish: function (data) {
-    data.correct = jsPsych.pluginAPI.compareKeys(
-      data.response,
-      data.correct_response
-    );
-    data.correct_response_meaning = correctKey === data.correct_response ? "YES" : "NO";
-  },
-};
-
-/* Test procedure: fixation + object presentation */
-let test_objects_procedure = {
-  timeline: [fixation, onlyFace, testObjects],
-  timeline_variables: test_objects_stimuli,
-  randomize_order: true, // Randomize object order
-};
-timeline.push(test_objects_procedure);
 
 
 /**************************************************************************************/
@@ -541,7 +267,7 @@ let tetris = {
   `,
   post_trial_gap: 500,
   choices: "NO_KEYS", // Prevent key press
-  trial_duration: 1200000, 
+  trial_duration: 100, 
 };
 timeline.push(tetris);
 
@@ -549,90 +275,35 @@ timeline.push(tetris);
 /**************************************************************************************/
 
 
-/* Instructions for faces presentation */
-let instructionsFacesPresentation = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: `
-    <p>Ahora realizará la siguiente tarea:</p>
-    <p>Si ha visto antes la cara, pulse la tecla '${correctKey.toUpperCase()}' (SÍ).</p>
-    <p>Si no ha visto antes la cara, pulse la tecla '${incorrectKey.toUpperCase()}' (NO).</p>
-    <p>De nuevo, le recomendamos colocar los dedos sobre las teclas ${correctKey.toUpperCase()} y ${incorrectKey.toUpperCase()} durante la tarea para no olvidarlas.</p>
-    <p>Pulse la barra espaciadora para comenzar.</p>
-  `,
-  choices: [' '],
-  post_trial_gap: 500,
-};
-timeline.push(instructionsFacesPresentation);
 
-/* Create stimuli array for faces presentation */
-let face_recognition_stimuli = recognitionFaces.map((face) => {
-  return {
-    stimulus: `
-      <div class="imgs-container">
-        <img class="person-img" src="${face.img}">
-      </div>
-      <div class="keys">
-        <p class="${correctKey === 'a' ? 'left' : 'right'}">SÍ</p>
-        <p class="${correctKey === 'a' ? 'right' : 'left'}">NO</p>
-      </div>
-    `,
-    correct_response: face.correct_response
-  };
-});
-
-/* Faces presentation trial */
-let testFaces = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: jsPsych.timelineVariable("stimulus"),
-  choices: ['a', 'l'],
-  data: {
-    task: "response faces test",
-    correct_response: jsPsych.timelineVariable("correct_response"),
-  },
-  on_finish: function (data) {
-    data.correct = jsPsych.pluginAPI.compareKeys(
-      data.response,
-      data.correct_response
-    );
-    data.correct_response_meaning = correctKey === data.correct_response ? "YES" : "NO";
-  },
-};
-
-/* Test procedure: fixation + faces presentation */
-let test_faces_procedure = {
-  timeline: [fixation, testFaces],
-  timeline_variables: face_recognition_stimuli,
-  randomize_order: true, // Randomize faces order
-};
-timeline.push(test_faces_procedure);
 
 
 // /**************************************************************************************/
 
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_API_KEY
-);
+// const supabase = createClient(
+//   import.meta.env.VITE_SUPABASE_URL,
+//   import.meta.env.VITE_SUPABASE_API_KEY
+// );
 
-const TABLE_NAME = "experimento_caras_atributos";
+// const TABLE_NAME = "";
 
-async function saveData(data) {
-  console.log(data);
-  const { error } = await supabase.from(TABLE_NAME).insert({ data });
+// async function saveData(data) {
+//   console.log(data);
+//   const { error } = await supabase.from(TABLE_NAME).insert({ data });
 
-  return { error };
-}
+//   return { error };
+// }
 
-const saveDataBlock = {
-  type: jsPsychCallFunction,
-  func: function() {
-    saveData(jsPsych.data.get())
-  },
-  timing_post_trial: 200
-}
+// const saveDataBlock = {
+//   type: jsPsychCallFunction,
+//   func: function() {
+//     saveData(jsPsych.data.get())
+//   },
+//   timing_post_trial: 200
+// }
 
-timeline.push(saveDataBlock);
+// timeline.push(saveDataBlock);
 
 
 
--- src/objects.js
+++ src/objects.js
@@ -1,272 +1,148 @@
 const IMG_URL =
-  "https://raw.githubusercontent.com/saramff/face-recognition-images/refs/heads/master/objects/";
+  "https://raw.githubusercontent.com/saramff/people-attributes-images/refs/heads/master";
 
-export const correctObjects = [
+const MEN_BODY_URL = `${IMG_URL}/hombres-experimentales`;
+const WOMEN_BODY_URL = `${IMG_URL}/mujeres-experimentales`;
+const MEN_FACE_URL = `${IMG_URL}/caras-antiguas-hombres`;
+const WOMEN_FACE_URL = `${IMG_URL}/caras-antiguas-mujeres`;
+
+export const menDataArray = [
   {
-    sentence: "tiene una hucha.", //"has a piggy bank.",
-    img: `${IMG_URL}piggy-bank.jpg`,
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene una papelera.", //"has a trash can.",
-    img: `${IMG_URL}trash-can.jpg`,
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene una cuerda.", //"has a rope.",
-    img: `${IMG_URL}rope.jpg`,
-    correct_response: "a"
+    bodyImg: `${MEN_BODY_URL}/man-01.jpg`,
+    faceImg: `${MEN_FACE_URL}/man-01.jpg`,
+    name: "",
+    trueSentence: "frase 1",
+    falseSentence: "",
+    correct_response: "",
   },
   {
-    sentence: "tiene una llave.", //"has a key.",
-    img: `${IMG_URL}key.jpg`,
-    correct_response: "a"
+    bodyImg: `${MEN_BODY_URL}/man-02.jpg`,
+    faceImg: `${MEN_FACE_URL}/man-02.jpg`,
+    name: "",
+    trueSentence: "frase 2",
+    falseSentence: "",
+    correct_response: "",
   },
   {
-    sentence: "tiene un paraguas.", //"has an umbrella.",
-    img: `${IMG_URL}umbrella.jpg`,
-    correct_response: "a"
+    bodyImg: `${MEN_BODY_URL}/man-03.jpg`,
+    faceImg: `${MEN_FACE_URL}/man-03.jpg`,
+    name: "",
+    trueSentence: "frase 3",
+    falseSentence: "",
+    correct_response: "",
   },
   {
-    sentence: "tiene unas gafas.", //"has glasses.",
-    img: `${IMG_URL}glasses.jpg`,
-    correct_response: "a"
+    bodyImg: `${MEN_BODY_URL}/man-04.jpg`,
+    faceImg: `${MEN_FACE_URL}/man-04.jpg`,
+    name: "",
+    trueSentence: "frase 4",
+    falseSentence: "",
+    correct_response: "",
   },
   {
-    sentence: "tiene un plátano.", //has a banana.",
-    img: `${IMG_URL}banana.jpg`,
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene una calculadora.", //"has a calculator.",
-    img: `${IMG_URL}calculator.jpg`,
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene unas tijeras.", //"has scissors.",
-    img: `${IMG_URL}scissors.jpg`,
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene un cepillo.", //"has a hairbrush.",
-    img: `${IMG_URL}hairbrush.jpg`,
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene una lupa.", 
-    img: `${IMG_URL}magnifying-glass.jpg`, 
-    correct_response: "a"
-  },
-  {
-    sentence: "tiene unas botas.", //"has some boots.",
-    img: `${IMG_URL}boots.jpg`,
-    correct_response: "a"
-  },
-  { 
-    sentence: "tiene una guitarra.", //"has a guitar.",
-    img: `${IMG_URL}guitar.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene miel.", //"has honey.", 
-    img: `${IMG_URL}honey.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un micrófono.", //has a microphone.", 
-    img: `${IMG_URL}microphone.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un martillo.", //has a hammer.", 
-    img: `${IMG_URL}hammer.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene leche.", //has milk.", 
-    img: `${IMG_URL}milk.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un huevo.", //has an egg.", 
-    img: `${IMG_URL}egg.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un perro.", //has a dog.", 
-    img: `${IMG_URL}dog.jpg`, 
-    correct_response: "a" },
-  { 
-    sentence: "tiene una naranja.", //has an orange.", 
-    img: `${IMG_URL}orange.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene pan.", //has bread.", 
-    img: `${IMG_URL}bread.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una bicicleta.", //has a bicycle.", 
-    img: `${IMG_URL}bicycle.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene café.", //has coffee.", 
-    img: `${IMG_URL}coffee.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un globo.", //has a balloon.", 
-    img: `${IMG_URL}balloon.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una pizza.", //has a pizza.", 
-    img: `${IMG_URL}pizza.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una piña.", //has a pineapple.", 
-    img: `${IMG_URL}pineapple.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una pantalla.", //has a screen.", 
-    img: `${IMG_URL}screen.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una pelota.", //has a ball.", 
-    img: `${IMG_URL}ball.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una mariposa.", //has a butterfly.", 
-    img: `${IMG_URL}butterfly.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene chocolate.", //has chocolate.", 
-    img: `${IMG_URL}chocolate.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una moneda.", //has a coin.", 
-    img: `${IMG_URL}coin.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un lápiz.", //has a pencil.", 
-    img: `${IMG_URL}pencil.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una silla.", //has a chair.", 
-    img: `${IMG_URL}chair.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un cinturón.", //has a belt.", 
-    img: `${IMG_URL}belt.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una cuchara.", //has a spoon.", 
-    img: `${IMG_URL}spoon.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un sombrero.", //has a hat.", 
-    img: `${IMG_URL}hat.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene queso.", //has cheese.", 
-    img: `${IMG_URL}cheese.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una maceta.", //has a flowerpot.", 
-    img: `${IMG_URL}flowerpot.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un batido.", //has a milkshake.", 
-    img: `${IMG_URL}milkshake.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una flor.", //has a flower.", 
-    img: `${IMG_URL}flower.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un reloj de pulsera.", //has a watch.", 
-    img: `${IMG_URL}watch.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un tomate.", //has a tomato.", 
-    img: `${IMG_URL}tomato.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una calabaza.", //has a pumpkin.", 
-    img: `${IMG_URL}pumpkin.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene un dado.", //has a dice.", 
-    img: `${IMG_URL}dice.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una regadera.", //has a watering can.", 
-    img: `${IMG_URL}watering-can.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una libreta.", //has a notebook.", 
-    img: `${IMG_URL}notebook.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una manzana.", //has an apple.", 
-    img: `${IMG_URL}apple.jpg`, 
-    correct_response: "a" 
-  },
-  { 
-    sentence: "tiene una regla.", //has a ruler.", 
-    img: `${IMG_URL}ruler.jpg`, 
-    correct_response: "a" 
+    bodyImg: `${MEN_BODY_URL}/man-05.jpg`,
+    faceImg: `${MEN_FACE_URL}/man-05.jpg`,
+    name: "",
+    trueSentence: "frase 5",
+    falseSentence: "",
+    correct_response: "",
   },
 ];
 
-export const incorrectObjects = [
-  "tiene una lámpara.",
-  "tiene una alfombra.",
-  "tiene una nevera.",
-  "tiene una sartén.",
-  "tiene una almohada.",
-  "tiene una jaula.",
-  "tiene una escalera.",
-  "tiene un bastón.",
-  "tiene una botella.",
-  "tiene un ventilador.",
-  "tiene una maleta.",
-  "tiene una impresora.",
-  "tiene una cartera.",
-  "tiene una linterna.",
-  "tiene una corbata.",
-  "tiene una plancha.",
-  "tiene un casco.",
-  "tiene una mochila.",
-  "tiene un espejo.",
-  "tiene una grapadora.",
-  "tiene un candado.",
-  "tiene una escoba.",
-  "tiene un coche.",
-  "tiene una pala.",
-  "tiene un mapa.",
+export const womenDataArray = [
+  {
+    bodyImg: `${WOMEN_BODY_URL}/woman-01.jpg`,
+    faceImg: `${WOMEN_FACE_URL}/woman-01.jpg`,
+    name: "",
+    trueSentence: "frase 01",
+    falseSentence: "",
+    correct_response: "",
+  },
+  {
+    bodyImg: `${WOMEN_BODY_URL}/woman-02.jpg`,
+    faceImg: `${WOMEN_FACE_URL}/woman-02.jpg`,
+    name: "",
+    trueSentence: "frase 02",
+    falseSentence: "",
+    correct_response: "",
+  },
+  {
+    bodyImg: `${WOMEN_BODY_URL}/woman-03.jpg`,
+    faceImg: `${WOMEN_FACE_URL}/woman-03.jpg`,
+    name: "",
+    trueSentence: "frase 03",
+    falseSentence: "",
+    correct_response: "",
+  },
+  {
+    bodyImg: `${WOMEN_BODY_URL}/woman-04.jpg`,
+    faceImg: `${WOMEN_FACE_URL}/woman-04.jpg`,
+    name: "",
+    trueSentence: "frase 04",
+    falseSentence: "",
+    correct_response: "",
+  },
+  {
+    bodyImg: `${WOMEN_BODY_URL}/woman-05.jpg`,
+    faceImg: `${WOMEN_FACE_URL}/woman-05.jpg`,
+    name: "",
+    trueSentence: "frase 05",
+    falseSentence: "",
+    correct_response: "",
+  },
+];
+
+// Create name arrays for men and women
+export const menNames = [
+  "Antonio",
+  "José",
+  "Manuel",
+  "Juan",
+  "Francisco",
+  "David",
+  "Javier",
+  "Carlos",
+  "Miguel",
+  "Jesús",
+  "Alejandro",
+  "Pedro",
+  "Pablo",
+  "Ángel",
+  "Sergio",
+  "Rafa",
+  "Andrés",
+  "Alberto",
+  "Enrique",
+  "Rubén",
+  "Fernando",
+  "Luis",
+  "Daniel",
+  "Marcos",
+];
+
+export const womenNames = [
+  "María",
+  "Carmen",
+  "Ana",
+  "Laura",
+  "Isabel",
+  "Marta",
+  "Sara",
+  "Elena",
+  "Lucía",
+  "Patricia",
+  "Teresa",
+  "Pilar",
+  "Silvia",
+  "Rosa",
+  "Beatriz",
+  "Claudia",
+  "Raquel",
+  "Sonia",
+  "Nuria",
+  "Alicia",
+  "Irene",
+  "Andrea",
+  "Cristina",
+  "Julia",
 ];
